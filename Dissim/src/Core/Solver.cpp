2461e782587a0698274aed0c65494995cbab1f60
Jelle Spijker <spijker.jelle@gmail.com>
2016-11-29 09:42:20 +0100

    Newton Solver added

--- Dissim/src/Core/Solver.cpp
+++ Dissim/src/Core/Solver.cpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <vector>
+#include <utility>
 
 #include <boost/make_shared.hpp>
 
@@ -41,8 +42,39 @@ Newton::~Newton() {
 }
 
 void Newton::Solve(Block::Block_ptr block, DissimType::Dissim_ptr var) {
+  bool saveSetting = block->getSaveHistory();
+  block->setSaveHistory(false);
+  DissimType::Value_t error;
+  error.resize(var->Value.rows(),var->Value.rows());
+  for (int i = 0; i < var->Value.rows(); i++){
+    for (int j = 0; j < var->Value.cols(); ++j) {
+      error(i,j) = 0.001;
+    }
+  }
 
+  bool optimized = false;
+  std::pair<DissimType::Dissim_ptr, DissimType::Dissim_ptr> xn;
+  xn.first = var;
+  xn.second = DissimType::Dissim_ptr(new DissimType);
 
+  while (!optimized) {
+    DissimType::Dissim_ptr f(block->Run());
+    DissimType::Dissim_ptr fPrime(diff(block, xn.first, 0.001));
+    DissimType::Value_t difference = f->Value * fPrime->Value.inverse();
+    xn.second->Value = xn.first->Value - difference;
+    DissimType::Value_t diffIter = xn.second->Value - xn.first->Value;
+    for (int i = 0; i < error.rows(); ++i) {
+      for (int j = 0; j < error.cols(); ++j) {
+        if (diffIter(i,j) < error(i,j)) {
+          optimized = true;
+        } else {
+          optimized = false;
+        }
+      }
+    }
+    xn.first->Value = xn.second->Value;
+  }
+  block->setSaveHistory(saveSetting);
 }
 
 }
